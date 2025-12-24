9bf8efe92c4db6c2ccccaa5c2dd8f3320fc78f19
Artyom Matashkov <aimatashkov@mai.education>
2025-12-24 18:40:43 +0300

    - Implement `LibraryPanel`

--- src/library/library.py
+++ src/library/library.py
@@ -56,16 +56,36 @@ class LibraryPanel(LibraryABC):
         self._library = library
 
     def add_book(self, book: Book):
-        pass
+        try:
+            self._library.add_book(book)
+            print(f"Добавлена книга: {book.title}")
+        except KeyError:
+            print(f"Книга с таким ISBN: {book.isbn} уже добавлена")
 
     def remove_book(self, book: Book):
-        pass
+        self._library.remove_book(book)
+        print(f"Удалена книга: {book.title}")
 
     def find_by_isbn(self, isbn: str) -> Book | None:
-        return None
+        book = self._library.find_by_isbn(isbn)
+        if book:
+            print(f"Найдена книга по ISBN: {book.title}")
+        else:
+            print("Книги с таким ISBN не найдено")
+        return book
 
     def find_by_author(self, author: str) -> ImmutableBookCollection:
-        return ImmutableBookCollection()
+        books = self._library.find_by_author(author)
+        if books:
+            print(f"Книги этого автора: {[book.title for book in books]}")
+        else:
+            print("Похоже, автор пока не нашёл свою музу...")
+        return books
 
     def find_by_year(self, year: int) -> ImmutableBookCollection:
-        return ImmutableBookCollection()
+        books = self._library.find_by_year(year)
+        if books:
+            print(f"Книги {year} года: {[book.title for book in books]}")
+        else:
+            print("Похоже, в тот год что-то произошло (Книги не найдены)")
+        return books
